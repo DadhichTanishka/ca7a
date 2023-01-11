b0cb7739b2efbd78b026a44ea6b9908712b03a76
unknown <tanishka98765@gmail.com>
2023-01-11 20:06:06 +0530

    done

---
+++ src/axios.js
@@ -0,0 +1,48 @@
+import { useState,useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+function Site(){
+    
+    const register=()=>{
+        <div>
+        console.log("hello")
+        {/* <div>hello</div> */}
+        </div>
+        
+    }
+    const[post,setpost]=useState(null);
+    const[searchs,newSearch]=useState("react");
+    const baseURL=`https://www.googleapis.com/books/v1/volumes?q=${searchs}`
+    useEffect(()=>{
+        axios.get(baseURL)
+        .then((response)=>{
+            console.log(response.data.items )
+            setpost(response.data.items)
+        })
+    },[searchs])
+    if(post!=null){
+        return(
+            <div id="main">
+                <div id="search">
+                    <input id="box" type="Text" placeholder="SEARCH"/>
+                    <button onClick={()=>newSearch(document.getElementById("box").value)} >🔍</button>
+                    {/* <button onClick={register}>register</button> */}
+                   <div id="li"><Link to="/form">register</Link></div> 
+                </div>
+                <div className="books">
+                    {post.map((e)=>
+                    <div className="booky">
+                    <h1>{e.volumeInfo.title}</h1>
+                    {e.volumeInfo.imageLinks?<img src={e.volumeInfo.imageLinks.thumbnail}></img>: <img src="https://onlinebookclub.org/book-covers/no-cover.jpg"/>}
+                    </div>
+                    )
+                    }
+                    
+                </div>
+            </div>
+        )
+    }
+    
+}
+export default Site
---
+++ src/registration.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Form() {
+    const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const { name, email, password, password2 } = formData;
+
+  const handleChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setErrors({}); // reset errors
+    let error=0;
+
+    // validate form fields
+    if (name.length <= 2) {
+      setErrors(errors => ({ ...errors, name: 'Name must be at least 2 characters' }));
+      error++;
+    }
+    if (!email.includes('@')) {
+      setErrors(errors => ({ ...errors, email: 'Email must be a valid address' }));
+      error++;
+    }
+    if (password !== password2) {
+      setErrors(errors => ({ ...errors, password: 'Passwords do not match' }));
+      error++;
+    }
+    if (error === 0) {
+       
+        navigate("/");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="name"
+        value={name}
+        onChange={handleChange}
+        placeholder="Name"
+      />
+      {errors.name?<p className="error">{errors.name}</p>:""}
+      <input
+        type="email"
+        name="email"
+        value={email}
+        onChange={handleChange}
+        placeholder="Email"
+      />
+      {errors.email? <p className="error">{errors.email}</p>:""}
+      <input
+        type="password"
+        name="password"
+        value={password}
+        onChange={handleChange}
+        placeholder="Password"
+      />
+      <input
+        type="password"
+        name="password2"
+        value={password2}
+        onChange={handleChange}
+        placeholder="Confirm Password"
+      />
+      {errors.password?<p className="error">{errors.password}</p>:""}
+      <button type="submit" >Submit</button>
+    </form>
+  );
+}
+export default Form;
+{/* <Navigate to="/dashboard" replace={true} /> */}
